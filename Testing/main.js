3565ae2e28bbc3205a41cb3e15655a75192cf8a6
Gamingregory112 <104397063+Gamingregory112@users.noreply.github.com>
2023-09-30 11:35:31 -0700

    Update main.js

--- Testing/main.js
+++ Testing/main.js
@@ -10,15 +10,3 @@ function getInfo() {
 		alert("Incorrect");
 	}
 }
-
-// function getInfo() {
-// 	var code = document.getElementById('code').value
-// 	let ServerProxy = atob("MTIzNDU2Nzg5MGJvYg==");
-// 	if(code == ServerProxy) {
-// 		window.location = "Game-Index.html"
-//         localStorage.setItem("LoggedIn", "true")
-// 		return
-// 	} else {
-// 		alert("incorrect code");
-// 	}
-// }
