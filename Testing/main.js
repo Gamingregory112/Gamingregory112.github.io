fae5e374923379e10d391e6e0cfc7e5aa67897b2
Gamingregory112 <104397063+Gamingregory112@users.noreply.github.com>
2024-01-23 18:31:14 -0800

    Update main.js

--- Testing/main.js
+++ Testing/main.js
@@ -1,7 +1,7 @@
-var ServerProxyValueU = ("Admin");
-var ServerProxyValueC = ("MTIzNDU2Nzg5MGJvYg==");
+window.ServerProxyValueU = ("Admin");
+window.ServerProxyValueC = ("MTIzNDU2Nzg5MGJvYg==");
 function getInfo() {
-	if(document.getElementById('code').value == atob(ServerProxyValueC) && document.getElementById('user').value == ServerProxyValueU) {
+	if(document.getElementById('code').value == atob(window.ServerProxyValueC) && document.getElementById('user').value == window.ServerProxyValueU) {
 		window.location = "games.html"
 		localStorage.setItem("ProxyStatus", "true")
 		localStorage.setItem("AlreadyProxy", "true")
