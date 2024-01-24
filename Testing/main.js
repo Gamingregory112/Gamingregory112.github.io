5b02ff4c71ae8c2dcf27b1615e4b9007087a475b
Gamingregory112 <104397063+Gamingregory112@users.noreply.github.com>
2024-01-24 10:56:31 -0500

    Update main.js

--- Testing/main.js
+++ Testing/main.js
@@ -1,17 +1,12 @@
-window.ServerProxyValueU = ("Admin");
-window.ServerProxyValueC = ("MTIzNDU2Nzg5MGJvYg==");
+var ServerProxyValueU = ("Admin");
+var ServerProxyValueC = ("MTIzNDU2Nzg5MGJvYg==");
 function getInfo() {
-	if(document.getElementById('code').value == atob(window.ServerProxyValueC) && document.getElementById('user').value == window.ServerProxyValueU) {
+	if(document.getElementById('code').value == atob(ServerProxyValueC) && document.getElementById('user').value == ServerProxyValueU) {
 		window.location = "games.html"
 		localStorage.setItem("ProxyStatus", "true")
 		localStorage.setItem("AlreadyProxy", "true")
 		return
 	} else {
-		if(document.getElementById('code').value == "Forgot" && document.getElementById('user').value == "Password") {
-			alert(atob(window.ServerProxyValueC));
-			return
-		} else {
 		alert("Incorrect");
-		}
 	}
 }
